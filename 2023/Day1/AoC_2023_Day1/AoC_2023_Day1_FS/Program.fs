cf07b53645308c4e447de055976f5679395b8c20
Lukas <lhuerlimann3@online.gibz.ch>
2023-12-03 00:05:26 +0100

    2023 Day 1 Part 1 with F#

--- 2023/Day1/AoC_2023_Day1/AoC_2023_Day1_FS/Program.fs
+++ 2023/Day1/AoC_2023_Day1/AoC_2023_Day1_FS/Program.fs
@@ -1,2 +1,18 @@
-﻿let number = 0
-let result = number + 1
+﻿open System
+open System.IO
+
+let input = File.ReadLines "../../../input.txt"
+
+let line = "heightseven4two5"
+
+let reverseString (input : string) = new String(input |> Seq.toArray |> Array.rev)
+
+let findDigit value = value |> Seq.find(fun (s : char) -> Char.IsDigit(s)) |> Char.ToString
+
+let findLastDigit = reverseString >> findDigit
+
+let concatStrings (firstString : string) (secondString : string) = firstString + secondString
+
+let result = input |> Seq.fold(fun x y -> x + (findDigit(y) + findLastDigit(y) |> int)) 0
+
+printfn "%A" result

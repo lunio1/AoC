0d36359c09675f56c629604bd00d3040d5ac6128
Lukas <lhuerlimann3@online.gibz.ch>
2023-12-17 18:55:20 +0100

    add DayTwoPartTwo FS

--- 2023/Day1/AoC_2023_Day1/AoC_2023_Day1_FS/DayOne/DayOne.fs
+++ 2023/Day1/AoC_2023_Day1/AoC_2023_Day1_FS/DayOne/DayOne.fs
@@ -1,18 +1,18 @@
 ﻿module DayOne
 
-open System
-open System.IO
+//open System
+//open System.IO
 
-let input = File.ReadLines "../../../DayOne/input.txt"
+//let input = File.ReadLines "../../../DayOne/input.txt"
 
-let reverseString (input : string) = new String(input |> Seq.toArray |> Array.rev)
+//let reverseString (input : string) = new String(input |> Seq.toArray |> Array.rev)
 
-let findDigit value = value |> Seq.find(fun (s : char) -> Char.IsDigit(s)) |> Char.ToString
+//let findDigit value = value |> Seq.find(fun (s : char) -> Char.IsDigit(s)) |> Char.ToString
 
-let findLastDigit = reverseString >> findDigit
+//let findLastDigit = reverseString >> findDigit
 
-let concatStrings (firstString : string) (secondString : string) = firstString + secondString
+//let concatStrings (firstString : string) (secondString : string) = firstString + secondString
 
-let result = input |> Seq.fold(fun x y -> x + (findDigit(y) + findLastDigit(y) |> int)) 0
+//let result = input |> Seq.fold(fun x y -> x + (findDigit(y) + findLastDigit(y) |> int)) 0
 
-let dayOnePartOne = printfn "%A" result
+//let dayOnePartOne = printfn "%A" result
--- 2023/Day1/AoC_2023_Day1/AoC_2023_Day1_FS/DayTwo/DayTwoPartTwo.fs
+++ 2023/Day1/AoC_2023_Day1/AoC_2023_Day1_FS/DayTwo/DayTwoPartTwo.fs
@@ -1,4 +1,4 @@
-﻿//module DayTwo
+﻿module DayTwoPartTwo
 
 open System
 open System.IO
